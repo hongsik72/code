abefab16eb9a986986c971bea4bd0ccb004ecaa7
hongsik72 <cokr011@naver.com>
2023-03-06 22:54:59 +0900

    00으로 수정

--- src/views/1_databinding/SocketView.vue
+++ src/views/1_databinding/SocketView.vue
@@ -10,8 +10,8 @@ export default {
   name: "HelloWorld",
   data() {
     return {
-      message: "upload3?",
-      message2: "upload4?",
+      message: "upload00",
+      message2: "upload00",
     }
   },
   created() {
